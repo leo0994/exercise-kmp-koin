cc1eb1f279abb7db41f72f44f50b4d9cb7c08700
lbarrantesm <lbarrantesm@ucenfotec.ac.cr>
2026-06-16 19:45:36 -0600

    leo ejercicio clase agregar koin

--- KMPKoin/shared/build.gradle.kts
+++ KMPKoin/shared/build.gradle.kts
@@ -48,6 +48,11 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.koin.compose.viewmodel.navigation)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
